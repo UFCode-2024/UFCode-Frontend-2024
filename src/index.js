4d2b187df2e037e4ee1892d8f389473913dd91e6
GuilhermeSousla <147992779+GuilhermeSousla@users.noreply.github.com>
2024-09-09 17:26:43 -0300

    login com google

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './config/store'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </Provider>,
   document.getElementById('root')
 );
